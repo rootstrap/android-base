63cedb635929b5ee148e8f0413f5e2b3fd790c78
rr <ricardo901130@gmail.com>
2019-07-04 15:50:54 -0300

    Add Klint to the project

--- app/src/main/java/com/rootstrap/android/lint/LintRegistry.kt
+++
@@ -1,7 +0,0 @@
-package com.rootstrap.android.lint
-
-import com.android.tools.lint.client.api.IssueRegistry
-import com.android.tools.lint.detector.api.Issue
-import java.util.*
-
-class LintRegistry(override val issues: List<Issue> = Collections.singletonList(ISSUE)) : IssueRegistry() { }
