149461c1acca45b976ada91235dd91a81d38cbef
rr <ricardo901130@gmail.com>
2019-11-19 13:13:52 -0300

    Fix comments

--- app/src/main/java/com/rootstrap/android/App.kt
+++ app/src/main/java/com/rootstrap/android/App.kt
@@ -3,9 +3,7 @@ package com.rootstrap.android
 import android.app.Application
 import com.rootstrap.android.metrics.Analytics
 import com.rootstrap.android.metrics.GoogleAnalytics
-import com.rootstrap.android.metrics.annotations.processVisits
 import com.rootstrap.android.metrics.appAnalytics
-import com.rootstrap.android.ui.activity.main.MainActivity
 
 class App : Application() {
     override fun onCreate() {
@@ -13,7 +11,6 @@ class App : Application() {
         Analytics.init()
 
         appAnalytics.addProvider(GoogleAnalytics(this))
-        processVisits(MainActivity::class)
         //You need the api key in order to use MixPanel
         // appAnalytics.addProvider(MixPanelAnalytics(applicationContext))
     }
