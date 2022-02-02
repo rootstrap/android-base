925539ed56c613fffd4e161ccf2c62d6a280f26d
aguskoll <aguskoll@gmail.com>
2022-02-02 14:17:34 -0300

    removed comment

--- app/src/main/java/com/rootstrap/android/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/rootstrap/android/ui/base/BaseViewModel.kt
@@ -1,6 +1,8 @@
 package com.rootstrap.android.ui.base
 
-import androidx.lifecycle.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.rootstrap.android.util.NetworkState
 
 /**
@@ -13,10 +15,4 @@ open class BaseViewModel : ViewModel() {
     protected val _networkState = MutableLiveData<NetworkState>()
     val networkState: LiveData<NetworkState>
         get() = _networkState
-
-  /*  @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
-    fun register() = Bus().register(this)
-
-    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
-    fun unregister() = Bus().unregister(this) */
 }
